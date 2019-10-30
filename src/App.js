e0b494bbee8cd1cfc14db2671bb7417846abe61f
PratibhaShrestha <pateebha@gmail.com>
2019-10-30 23:54:21 +0100

    Changed app.js to class component and used videolist and videodetail component with youtubeapi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import youtubeapi from "./api/youtubeapi";
+import VideoList from "./components/VideoList";
+import VideoDetail from "./components/VideoDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  onSearch = async () => {
+    const searchTerm = "trevor";
+
+    const response = await youtubeapi.get("/search", {
+      params: {
+        q: searchTerm
+      }
+    });
+
+    if (response !== null) {
+      console.log("response: ", response.data.items);
+      this.setState({
+        videos: response.data.items
+      });
+    }
+  };
+
+  handleVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "1em" }}>
+        <div className="ui grid">
+          <div className="row">
+            <button>Search</button>
+          </div>
+
+          <div className="ui row">
+            <div className="eleven wide column" onClick={this.onSearch}>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                handleVideoSelect={this.handleVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
