055d13de919dccef1bfc543712ccc2b327af586f
PratibhaShrestha <pateebha@gmail.com>
2019-10-30 23:51:15 +0100

    Added the components to list Videos and video details.

---
+++ src/api/youtubeapi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const KEY = "AIzaSyDZnhSlEDh0bpXzDyGYv2vbrcZbA5go3fQ";
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
