425df38102c5d05df187660ab0e4650333c60c49
PratibhaShrestha <pateebha@gmail.com>
2019-10-31 15:22:20 +0100

    changed the propType to object

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,20 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { renderHTML } from "../utils/helpers";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div>Click on an video to play...</div>;
+    return <div>Click on a video from list to play...</div>;
   }
 
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-  console.log(typeof video);
   return (
     <div>
       <div className="ui embed">
         <iframe src={videoSrc} allowFullScreen title="Video player" />
       </div>
       <div className="ui segment">
-        <h4 className="ui header">{video.snippet.title}</h4>
+        <h4 className="ui header">{renderHTML(video.snippet.title)}</h4>
         <p>{video.snippet.description}</p>
       </div>
     </div>
@@ -22,7 +22,7 @@ const VideoDetail = ({ video }) => {
 };
 
 VideoDetail.propTypes = {
-  video: PropTypes.array
+  video: PropTypes.object
 };
 
 export default VideoDetail;
