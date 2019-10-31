f36a5b157d761373fc046ad7157a68e214d6ccd6
PratibhaShrestha <pateebha@gmail.com>
2019-10-31 23:39:57 +0100

    Showing loading, when fetching data, also checking if there is any hiddensIds and fetching more videos on necessary, showing toast on errors And persisting the hiddensIDS in the localStorage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
 import React from "react";
+import { Dimmer, Loader } from "semantic-ui-react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import youtubeapi from "./api/youtubeapi";
+import { getVideosForChannel } from "./api/youtubeapi";
+import { STORAGE_BLACKLIST_IDS } from "./configs/constants";
+
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
 import SearchList from "./components/SearchList";
-import { STORAGE_BLACKLIST_IDS } from "./configs/constants";
 
 class App extends React.Component {
   state = {
     videos: [],
     selectedVideo: null,
-    blacklistIds: []
+    hiddenIds: [],
+    fetching: null
   };
 
   componentDidMount() {
     // CLEARING the localStorage for testing!
-    // localStorage.clear();
+    // Develop part START
+    /*
+    localStorage.clear();
+    toast.success("All hiddenIds are cleared from storage");
+    */
+    // Develop part END
 
     this.setState({
-      blacklistIds:
-        JSON.parse(localStorage.getItem(STORAGE_BLACKLIST_IDS)) || []
+      hiddenIds: JSON.parse(localStorage.getItem(STORAGE_BLACKLIST_IDS)) || []
     });
   }
 
   onSearch = channelIDs => {
-    this.setState({ videos: [] }, () => {
+    this.setState({ videos: [], fetching: true }, () => {
       this._searchVideosForChannelId(channelIDs);
     });
   };
 
   onHandleClick = video => {
-    console.log("onHandleClidk here ", video);
     this.setState(
       prevState => ({ selectedVideo: video }),
       () => {
@@ -39,20 +47,50 @@ class App extends React.Component {
     );
   };
 
-  _searchVideosForChannelId = channelIDs => {
-    channelIDs.forEach(async channelId => {
-      const response = await youtubeapi.get("/search", {
-        params: {
-          channelId: channelId,
-          order: "date"
-        }
-      });
-
-      if (response !== null) {
+  // this should be done as promise again, since it is repeated twice.
+  _getVideoForChannel = (channelId, maxResults) => {
+    getVideosForChannel(channelId, maxResults)
+      .then(videos => {
         this.setState(prevState => ({
-          videos: [...prevState.videos, ...response.data.items]
+          videos: [...prevState.videos, ...videos],
+          fetching: false
         }));
-      }
+      })
+      .catch(error => {
+        toast.dismiss();
+        toast.error(error.message);
+        this.setState({ fetching: false });
+      });
+  };
+
+  _searchVideosForChannelId = channelIDs => {
+    if (channelIDs === null || channelIDs.length === 0) {
+      this.setState({ fetching: false });
+      return;
+    }
+    channelIDs.forEach(channelId => {
+      getVideosForChannel(channelId, 10)
+        .then(videos => {
+          // checking if the videos are in hiddenList or not!
+          const hiddenCount = videos.filter(video =>
+            this.state.hiddenIds.some(hidden => hidden === video.id.videoId)
+          );
+          // if any videos are hidden, load the remainig number again + 10...
+          if (hiddenCount.length > 0) {
+            this._getVideoForChannel(channelId, 10 + hiddenCount.length);
+          } else {
+            // if nothing is hidden, then store the videos.. otherwise , all videos will be saved in _getVideoForChannel
+            this.setState(prevState => ({
+              videos: [...prevState.videos, ...videos],
+              fetching: false
+            }));
+          }
+        })
+        .catch(error => {
+          toast.dismiss();
+          toast.error(error.message);
+          this.setState({ fetching: false });
+        });
     });
   };
 
@@ -60,17 +98,21 @@ class App extends React.Component {
     if (videoId && videoId !== null) {
       this.setState(
         prevState => ({
-          blacklistIds: [...prevState.blacklistIds, videoId]
+          hiddenIds: [...prevState.hiddenIds, videoId]
         }),
         () => {
           localStorage.setItem(
             STORAGE_BLACKLIST_IDS,
-            JSON.stringify(this.state.blacklistIds)
+            JSON.stringify(this.state.hiddenIds)
           );
         }
       );
     } else {
       console.error("videoId is null !");
+      toast.dismiss();
+      toast.error(
+        "Cannot perform this action. It might be playlist or unplayable."
+      );
     }
   };
 
@@ -88,11 +130,17 @@ class App extends React.Component {
               className="six wide column"
               style={{ backgroundColor: "#fafafa", padding: "1.2em" }}
             >
+              {this.state.fetching && (
+                <Dimmer active inverted>
+                  <Loader inverted>Loading</Loader>
+                </Dimmer>
+              )}
+
               <VideoList
                 videos={this.state.videos}
                 onCloseClick={this.onBlackListVideo}
                 handleVideoSelect={this.onHandleClick}
-                blacklistIds={this.state.blacklistIds}
+                hiddenIds={this.state.hiddenIds}
               />
             </div>
 
@@ -104,6 +152,7 @@ class App extends React.Component {
             </div>
           </div>
         </div>
+        <ToastContainer autoClose={2500} />
       </div>
     );
   }
