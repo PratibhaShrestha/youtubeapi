55d62f9a664bb319cfa02a512f6399fc870438ea
PratibhaShrestha <pateebha@gmail.com>
2019-10-31 14:08:03 +0100

    format DateTime method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,17 @@
-import { React } from "react";
+import React from "react";
 
 export const renderHTML = escapedHTML =>
   React.createElement("div", {
     dangerouslySetInnerHTML: { __html: escapedHTML }
   });
+
+export const formatDate = strDate => {
+  return new Intl.DateTimeFormat("no-NO", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+
+    hour: "numeric",
+    minute: "numeric"
+  }).format(new Date(strDate));
+};
